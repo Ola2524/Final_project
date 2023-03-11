0174a6f73be1b8b8a1303ac6eeae9a9cd552b5f8
Ola2524 <o.mohamed.mmb@gmail.com>
2023-03-11 19:18:08 +0200

    edit contact us

--- resources/views/user/contact.blade.php
+++ resources/views/user/contact.blade.php
@@ -3,13 +3,14 @@
     <!-- Contact Start -->
     <div class="container-xxl py-5">
         <div class="container">
-            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
-                <h6 class="section-title bg-white text-center text-primary px-3">Contact Us</h6>
-                <h1 class="display-6 mb-4">If You Have Any Query, Please Feel Free Contact Us</h1>
-            </div>
-            <div class="row g-0 justify-content-center">
-                <div class="col-lg-8 wow fadeInUp" data-wow-delay="0.5s">
-                    
+            <div class="row g-5">
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="img-border">
+                        <img class="img-fluid" src="{{asset('img/babysitter.jpg')}}" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="h-100">
             <form method="POST" action="{{ route('contact_us') }}" id="contactUSForm">
                 @csrf
                 @if(session('message'))
@@ -63,14 +64,15 @@
             </div>
         </div>
     </div>
+    </div>
     <!-- Contact End -->
 
 
     <!-- Google Map Start -->
-    <div class="container-xxl pt-5 px-0 wow fadeIn" data-wow-delay="0.1s">
+    {{-- <div class="container-xxl pt-5 px-0 wow fadeIn" data-wow-delay="0.1s">
         <iframe class="w-100 mb-n2" style="height: 450px;"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
             frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-    </div>
+    </div> --}}
     <!-- Google Map End -->
 @endsection
